b25d12d59423d1d50089296bacffc013bc0d84a9
Christopher Culkeen <chucksculkeen@yahoo.ie>
2016-08-25 19:51:37 +0000

    cart_working

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -61,7 +61,7 @@ class BikesController < ApplicationController
     end
   end
 
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_bike
       @bike = Bike.find(params[:id])
